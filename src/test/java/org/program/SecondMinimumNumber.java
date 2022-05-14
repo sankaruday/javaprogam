19c4e3da6b3b4e9b1e7fa81f1b1713e58bd5d523
sankaruday <sankaruday87@gmail.com>
2022-05-14 11:59:39 +0530

    uday coding

---
+++ src/test/java/org/program/SecondMinimumNumber.java
@@ -0,0 +1,25 @@
+package org.program;
+
+public class SecondMinimumNumber {
+
+	public static void main(String[] args) {
+
+		int a[]={-12,45,-23,64,-100,24};
+		for(int i=0;i<a.length;i++){
+		for(int j=i+1;j<a.length;j++){
+		int temp=0;
+		if(a[i]<a[j]){
+		temp=a[j];
+		a[j]=a[i];
+		a[i]=temp;
+		}
+		}
+		}
+		for(int k=0;k<a.length;k++){
+		System.out.println(a[k]);
+		}
+		System.out.println("The Second minimum number is" +
+		a[a.length-2] );
+	}
+
+}
